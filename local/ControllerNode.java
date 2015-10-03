b17770c7697914272407d84172ebb05a2c71852d
Ronak Shah <ronakshah725@gmail.com>
2015-10-03 18:00:57 -0500

    added local

---
+++ local/ControllerNode.java
@@ -0,0 +1,166 @@
+package local;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.SynchronousQueue;
+
+public class ControllerNode {
+
+	Integer NodeID;
+	String HOSTNAME;
+	int PORT = 9011;
+	ServerSocket controllerSocket;
+
+	SynchronousQueue<InMessage> q = new SynchronousQueue<>();
+
+	static ConcurrentHashMap<Integer, PrintWriter> outMap = new ConcurrentHashMap<Integer, PrintWriter>();
+
+	static ConcurrentHashMap<Integer, Boolean> checkStartMap = new ConcurrentHashMap<>();
+
+	ControllerNode(int nodeID) {
+		this.NodeID = nodeID;
+		try {
+			this.HOSTNAME = InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public String toString() {
+
+		return NodeID + "@" + HOSTNAME + ":" + PORT;
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		ControllerNode controller = new ControllerNode(11);
+		try {
+
+			controller.controllerSocket = new ServerSocket(controller.PORT);
+
+			int upNodes;
+			while (true) {
+				System.out.println("Starting to accept");
+				new controllerRequestHandler(controller.controllerSocket.accept()).start();
+	
+				if((upNodes = checkStartMap.size()) == 1){
+
+					break;
+				}
+			}
+			
+			System.out.println("Out of while outMap: "+ outMap);
+			System.exit(1);
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			while (!(upNodes == 10)) {
+				// broadcast all nodes to start
+
+				for (PrintWriter pw : outMap.values()) {
+
+					pw.println("establish");
+				}
+
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		finally {
+
+			try {
+				controller.controllerSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	@SuppressWarnings("unused")
+	private static boolean isStopped() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	static class controllerRequestHandler extends Thread {
+
+		private Integer clientNodeId;
+		private Socket socket;
+		private BufferedReader in;
+		private PrintWriter out;
+
+		public controllerRequestHandler(Socket cSocket) {
+			this.socket = cSocket;
+		}
+
+		public void run() {
+			try {
+				System.out.println("In Request Handler for Client :" + socket);
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				out = new PrintWriter(socket.getOutputStream(), true);
+				String inputString;
+
+				while (true) {
+					inputString = in.readLine();
+					System.out.println("Value from client" + inputString);
+					clientNodeId = Integer.parseInt(inputString.split("#")[0]);
+
+					if (clientNodeId > 0 && clientNodeId < 11) {
+						out.println("ok");
+						System.out.println("client ok sent");
+						break;
+
+					}
+
+				}
+				System.out.println("Out of while");
+				checkStartMap.put(clientNodeId, true);
+				outMap.put(clientNodeId, out);
+
+			} catch (IOException e) {
+				System.out.println(e);
+			} finally {
+				
+				try {
+					socket.close();
+					out.close();
+//					System.exit(0);
+					System.out.println("closed");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		}
+
+	}
+
+}
---
+++ local/Node02.java
@@ -0,0 +1,183 @@
+package local;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.SynchronousQueue;
+
+
+public class Node02 {
+
+
+	
+	
+	Integer NodeID;
+	String HOSTNAME;
+	int noOfNodes = 10;
+	int PORT;
+	int BasePort = 9000;
+	String controllerHostName = "dc11.utdallas.edu";
+	
+	SynchronousQueue<InMessage> q = new SynchronousQueue<>();
+	ConcurrentHashMap<Integer, PrintWriter> outMap = new ConcurrentHashMap<>();
+	
+	
+	boolean startConnectingToOtherNodes = false;
+	static boolean higherConnDone=false;
+
+	Node02(int nodeID) {
+		
+		this.NodeID = nodeID;
+		try {
+			this.HOSTNAME = InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		PORT = BasePort + nodeID;
+
+	}
+
+	public String toString() {
+
+		return NodeID + "@" + HOSTNAME + ":" + PORT;
+	}
+	
+	 String getNodeAddress(int id) {
+		if(id<10)
+		{
+		
+			return "dc0"+id+".utdallas.edu";
+		}
+		else {
+			return "dc"+id+".utdallas.edu";
+		}
+
+	}
+	 
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		Node02 me = new Node02(02);
+		System.out.println("Node" + me + " is running.");
+		
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		
+																   @SuppressWarnings("resource")
+		ServerSocket listener = new ServerSocket(me.PORT);
+		
+		Thread.sleep(1000);
+														@SuppressWarnings("resource")
+		Socket contSock = new Socket(me.HOSTNAME, 9011);
+		PrintWriter outCont = new PrintWriter(contSock.getOutputStream());
+		BufferedReader inFromController = new BufferedReader(new InputStreamReader(contSock.getInputStream()));
+		outCont.println(me.NodeID +"#"+"up");
+		System.out.println("sent up");
+		Thread.sleep(1000);
+		System.exit(1);
+		while(!me.startConnectingToOtherNodes){
+			String msg = inFromController.readLine();
+			if (msg.contains("establish")){
+				me.startConnectingToOtherNodes = true;
+			}	
+		}
+		
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+		while (!higherConnDone) {
+			me.new RequestHandler(listener.accept()).start();
+			if(me.outMap.size()==(me.noOfNodes-me.NodeID)) //for node 9 this will be 0, so it will exit while instantly
+					{
+						higherConnDone = true;
+					}
+					
+		}
+
+		
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+			if (me.startConnectingToOtherNodes) {  
+
+				for (int id = me.NodeID-1; id > 0; id--) {
+					me.new RequestHandler(new Socket(me.HOSTNAME, me.BasePort+id));
+					
+
+				}
+			}
+			
+			System.out.println("Done connections to all");
+			System.out.println(me.outMap);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			
+			
+
+		
+
+
+
+	}
+
+
+
+	@SuppressWarnings("unused")
+	private boolean isStopped() {
+		// TODO Auto-generated method stub
+		
+		return false;
+	}
+
+	class RequestHandler extends Thread {
+
+		private Integer clientNodeId; // id for which it is a handler
+
+		private Socket socket;
+		private BufferedReader in;
+
+		public RequestHandler(Socket cSocket) {
+			// TODO Auto-generated constructor stub\
+			this.socket = cSocket;
+			clientNodeId = (Integer) (cSocket.getPort() - 9000);
+		}
+
+
+		public void run() {
+			try {
+
+				System.out.println("In Request Handler for Client :" + clientNodeId);
+				setIn(new BufferedReader(new InputStreamReader(socket.getInputStream())));
+				
+				outMap.put(clientNodeId, new PrintWriter(socket.getOutputStream()) );
+				
+				
+		
+			} catch (IOException e) {
+				System.out.println(e);
+			} finally {
+	
+
+			}
+		}
+
+
+		public BufferedReader getIn() {
+			return in;
+		}
+
+
+		public void setIn(BufferedReader in) {
+			this.in = in;
+		}
+
+	}
+
+
+
+}
